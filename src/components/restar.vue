6a5bc6523f348245d71e553ba303b72b604ca6dc
Aythami de Armas <aydearcas@gmail.com>
2021-03-16 20:26:00 +0000

    Initial Commit

---
+++ src/components/restar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="resta">Restar</button>
+    <span> Resulado:{{ result }} </span>
+  </div>
+</template>
+<script>
+import { EventBus } from '../event-bus.js'
+export default {
+  name: 'Restar',
+  props: {
+    result: {
+      type: Number,
+      default: 0
+    }
+  },
+  /* data() {
+      return {
+        result: 0
+      }
+  }, */
+  methods: {
+    resta () {
+      this.result--
+      EventBus.$emit('updateResult', this.result)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/sumar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="suma">Sumar</button>
+    <span> Resulado:{{ result }} </span>
+  </div>
+</template>
+<script>
+import { EventBus } from '../event-bus.js'
+export default {
+  name: 'Sumar',
+  props: {
+    result: {
+      type: Number,
+      default: 0
+    }
+  },
+  /* data() {
+      return {
+        result: 0
+      }
+  }, */
+  methods: {
+    suma () {
+      this.result++
+      EventBus.$emit('updateResult', this.result)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
